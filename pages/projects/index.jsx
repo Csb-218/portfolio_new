ad24cfa0ae6568f14c02adf5008e9e35e2d83b6a
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-02-27 02:47:49 +0530

    Update index.jsx

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -7,7 +7,7 @@ import Color  from '../../components/utils/page.colors.util'
 import settings from '../../content/_settings.json'
 import colors from '../../content/projects/_colors.json'
 
-//
+
 export default function Projects({ user, repos }) {
 	return (
 		<>
