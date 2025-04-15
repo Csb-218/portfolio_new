5ada9e9054f0e4cac402570e4e1ee3623180e79c
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-04-16 02:36:05 +0530

    feat : added links to terms and contact us

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Link from 'next/link';
 
 import Container from '../structure/container'
 import Icon from '../utils/icon.util'
@@ -63,60 +64,16 @@ export default function Footer() {
 						</li>
 					</ul>
 
-				</section>
-				{/* <section className={css.sections}>
-					<ul className={css.thanks}>
-						<li><h4>Acknowledgments</h4></li>
-						{
-						content.acknowledgments.map( ({ person, link, note }, index) => {
-							return (
-								<li key={index}>
-									<a href={link} rel="noreferrer" target="_blank">{person} <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
-									<p>{note}</p>
-								</li>
-							)
-						})
-						}
-					</ul>
-					<ul className={css.links}>
-						<li><h4>Links</h4></li>
-						{
-						content.links.map( ({ person, link, note }, index) => {
-							return (
-								<li key={index}>
-									<a href={link} rel="noreferrer" target="_blank">{person} <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
-									<p>{note}</p>
-								</li>
-							)
-						})
-						}
-					</ul>
-					<ul className={css.social}>
-						<li><h4>Social</h4></li>
-						<li className={css.socialList}>
-							{
-							content.social.map( ({ url, icon }, index) => {
-								return (
-									<a  key={index} href={url} rel="noreferrer" target="_blank"><Icon icon={[ 'fab', icon ]} /></a>
-								)
-							})
-							}
+					<ul className={css.footerLinks}>
+						<li>
+							<Link href="/terms">Terms & Conditions</Link>
+						</li>
+						<li>
+							<Link href="/contactUs">Contact Us</Link>
 						</li>
 					</ul>
-				</section> */}
-				{/* <section className={css.github}>
-					<a href={settings.portfolio.repo_html} rel="noreferrer" target="_blank">
-						<h5>{settings.portfolio.forkthis}</h5>
-						<ul>
-							<li>
-								<p><Icon icon={[ 'fad', 'code-branch' ]} /> Forks: { gitHubInfo.forks }</p>
-							</li>
-							<li>
-								<p><Icon icon={[ 'fad', 'star' ]} /> Stars: { gitHubInfo.stars }</p>
-							</li>
-						</ul>
-					</a>
-				</section> */}
+
+				</section>
 			</Container>
 			<canvas id="gradient-canvas" className={''} data-transition-in ></canvas>
 		</footer>
