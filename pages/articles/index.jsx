ef211b9cb1992605bfa7d825c438a70f379309e3
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-09-03 00:00:58 +0530

    fix : Resolved article thumbnail display
    Replaced API source

--- pages/articles/index.jsx
+++ pages/articles/index.jsx
@@ -25,15 +25,52 @@ export async function getServerSideProps({ res }) {
 		'public, s-maxage=600, stale-while-revalidate=59'
 	)
 
-	console.log(settings.username.medium)
 
-	const [ mediumRSS ] = await Promise.all( [
-		fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${settings.username.medium}`),
-	] )
+	// const [ mediumRSS ] = await Promise.all( [
+	// 	fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${settings.username.medium}`),
+	// ] )
 	
-	let [ mediumArticles ] = await Promise.all( [
-		mediumRSS.json(),
-	] )
+	// let [ mediumArticles ] = await Promise.all( [
+	// 	mediumRSS.json(),
+	// ] )
 
-	return { props: { mediumArticles } }
+	const host = process.env.X_Rapidapi_Host
+	const api_key = process.env.X_Rapidapi_Key
+	const user_id = process.env.Rapid_Medium_User_Id
+ 
+
+	const [userArticles] = await Promise.all([
+		
+		fetch(`https://${host}/user/${user_id}/articles`,{
+			method:"GET",
+			headers:{
+				"Host" : host ,
+				"X-Rapidapi-Host" : host ,
+				"X-Rapidapi-Key" : api_key
+			}
+
+		})
+	])
+
+	const [article_ids] = await Promise.all([
+		userArticles.json()
+	])
+
+	const mediumArticles = await Promise.all(
+		article_ids?.associated_articles?.map(id =>
+			fetch(`https://${host}/article/${id}`, {
+				method: "GET",
+				headers: {
+					"Host": host,
+					"X-Rapidapi-Host": host,
+					"X-Rapidapi-Key": api_key
+				}
+			}).then(res => res.json())
+		)
+	)
+
+
+	return {
+		props: { mediumArticles }
+	}
 }
